9b899d026f53b05d3e6f055cae71d677b3cfe5bd
Rolly Agsalog <rollyagsalog@gmail.com>
2013-11-19 18:39:05 -0800

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Samudra::Application.config.secret_key_base = '3286c035a11dec4ab3490d46444f88c7cda38e00756bc5c38efa1ccb3df32d1c193c422f6ea4c3defcd2470af24a6c25f5509177095ac58b469a3ccae1b61bbf'
